bb41842aeb4074997b9d6ab03cdc384c82fde691
Shahid N. Shah <shahid@shah.org>
2021-09-16 16:46:47 -0400

    chore: upgrade dependencies

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
-export * as base64 from "https://deno.land/std@0.106.0/encoding/base64.ts";
-export * as colors from "https://deno.land/std@0.106.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.106.0/fs/mod.ts";
-export * as io from "https://deno.land/std@0.106.0/io/mod.ts";
-export * as log from "https://deno.land/std@0.106.0/log/mod.ts";
-export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
-export * as yaml from "https://deno.land/std@0.106.0/encoding/yaml.ts";
-export * as uuid from "https://deno.land/std@0.106.0/uuid/mod.ts";
+export * as base64 from "https://deno.land/std@0.107.0/encoding/base64.ts";
+export * as colors from "https://deno.land/std@0.107.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.107.0/fs/mod.ts";
+export * as io from "https://deno.land/std@0.107.0/io/mod.ts";
+export * as log from "https://deno.land/std@0.107.0/log/mod.ts";
+export * as path from "https://deno.land/std@0.107.0/path/mod.ts";
+export * as yaml from "https://deno.land/std@0.107.0/encoding/yaml.ts";
+export * as uuid from "https://deno.land/std@0.107.0/uuid/mod.ts";
 
 export * as mediaTypes from "https://denopkg.com/oakserver/media_types@v2.10.1/mod.ts";
 export * as events from "https://denopkg.com/ihack2712/eventemitter@1.2.3/mod.ts";
--- lib/db/deps.ts
+++ lib/db/deps.ts
@@ -1,4 +1,4 @@
 export * as dotenv from "https://deno.land/x/dotenv@v3.0.0/mod.ts";
 export * as pg from "https://deno.land/x/postgres@v0.12.0/mod.ts";
 export * as pgQuery from "https://deno.land/x/postgres@v0.12.0/query/query.ts";
-export * as redis from "https://deno.land/x/redis@v0.23.2/mod.ts";
+export * as redis from "https://deno.land/x/redis@v0.24.0/mod.ts";
