a578dff1a2a8de5fcdb85a1dabba4107c0276c76
Shahid N. Shah <shahid@shah.org>
2021-10-02 12:45:18 -0400

    maintain: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,14 +1,14 @@
-export * as base64 from "https://deno.land/std@0.108.0/encoding/base64.ts";
-export * as colors from "https://deno.land/std@0.108.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.108.0/fs/mod.ts";
-export * as io from "https://deno.land/std@0.108.0/io/mod.ts";
-export * as log from "https://deno.land/std@0.108.0/log/mod.ts";
-export * as path from "https://deno.land/std@0.108.0/path/mod.ts";
-export * as yaml from "https://deno.land/std@0.108.0/encoding/yaml.ts";
-export * as uuid from "https://deno.land/std@0.108.0/uuid/mod.ts";
+export * as base64 from "https://deno.land/std@0.109.0/encoding/base64.ts";
+export * as colors from "https://deno.land/std@0.109.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.109.0/fs/mod.ts";
+export * as io from "https://deno.land/std@0.109.0/io/mod.ts";
+export * as log from "https://deno.land/std@0.109.0/log/mod.ts";
+export * as path from "https://deno.land/std@0.109.0/path/mod.ts";
+export * as yaml from "https://deno.land/std@0.109.0/encoding/yaml.ts";
+export * as uuid from "https://deno.land/std@0.109.0/uuid/mod.ts";
 
 export * as events from "https://denopkg.com/ihack2712/eventemitter@1.2.3/mod.ts";
-export * as json5 from "https://cdn.skypack.dev/json5@v2.2.0";
+export * as json5 from "https://cdn.skypack.dev/json5@v2.2.0/";
 export * as mediaTypes from "https://denopkg.com/oakserver/media_types@v2.10.2/mod.ts";
 
 export * as safety from "https://denopkg.com/shah/ts-safety@v1.0.2/mod.ts";
