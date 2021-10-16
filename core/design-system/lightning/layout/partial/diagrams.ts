58b1a77694dd5a0ff850705894e09c35f6e4683e
Shahid N. Shah <shahid@shah.org>
2021-10-16 17:10:39 -0400

    feat: implement diagram contributions scripts

--- core/design-system/lightning/layout/partial/diagrams.ts
+++ core/design-system/lightning/layout/partial/diagrams.ts
@@ -1,24 +1,41 @@
 import * as ldsGovn from "../../governance.ts";
 
 const mermaid = "mermaid";
-const isMermaidDiagrams = (diagrams: unknown) => {
-  if (typeof diagrams === "string" && diagrams == mermaid) return true;
-  if (Array.isArray(diagrams) && diagrams.find((name) => name == mermaid)) {
-    return true;
+const kroki = "kroki";
+
+function diagramScripts(fmProperty: unknown): string {
+  const html: string[] = [];
+  const specs: unknown[] = Array.isArray(fmProperty)
+    ? fmProperty
+    : [fmProperty];
+  for (const spec of specs) {
+    if (typeof spec === "string") {
+      switch (spec) {
+        case mermaid:
+          html.push(
+            `<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>\n<script>mermaid.initialize({startOnLoad:true});</script>`,
+          );
+          break;
+
+        case kroki:
+          // TODO: fix /lighting so that it uses baseURL
+          html.push(
+            `<script type="module" src="/lightning/component/kroki-diagram.js"></script>`,
+          );
+          break;
+      }
+    }
   }
-  return false;
-};
+  return html.join("\n");
+}
 
 /**
  * Place this in the <head> of your layouts to automatically include diagrams
  * generator code when layout.frontmatter.diagrams is true.
- * blank if not.
- * @param _ body is not used
  * @param layout the active layout strategy execution instance
  * @returns diagrams scripts for <head> component
  */
 // deno-fmt-ignore (because we don't want ${...} wrapped)
-export const mermaidDiagramsPartial: ldsGovn.LightningPartial = (layout) => 
-(layout?.frontmatter?.diagrams && isMermaidDiagrams(layout.frontmatter.diagrams)) 
-  ? `<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>\n<script>mermaid.initialize({startOnLoad:true});</script>`
-  : '<!-- layout.frontmatter.diagrams is false -->';
+export const clientDiagramsContributionsPartial: ldsGovn.LightningPartial = (layout) => 
+(layout?.frontmatter?.diagrams ? diagramScripts(layout.frontmatter.diagrams) 
+  : '<!-- layout.frontmatter.diagrams is false -->');
--- core/design-system/lightning/layout/partial/typical.ts
+++ core/design-system/lightning/layout/partial/typical.ts
@@ -56,7 +56,7 @@ export const typicalHeadPartial: ldsGovn.LightningPartial = (layout) => `
      and dependency manager. You should use this instead of <script> tags.
      TODO: consider https://addyosmani.com/basket.js/ as well -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/script.js/2.5.9/script.min.js"></script>
-${dia.mermaidDiagramsPartial(layout)}
+${dia.clientDiagramsContributionsPartial(layout)}
 ${clientCargoPartial(layout)} 
 <link rel="shortcut icon" href="${layout.dsArgs.assets.favIcon("/asset/image/favicon.ico")}"/>
 <title>${layout.layoutText.title(layout)}</title>
