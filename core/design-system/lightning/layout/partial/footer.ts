95aa086d1edf157e88f743a3231cbc57aaf14d55
Shahid N. Shah <shahid@shah.org>
2021-10-17 09:55:25 -0400

    fix: footer is now in a different file

--- core/design-system/lightning/layout/partial/footer.ts
+++ core/design-system/lightning/layout/partial/footer.ts
@@ -3,7 +3,7 @@ import * as ldsGovn from "../../governance.ts";
 export const footerFixedCopyrightBuildPartial: ldsGovn.LightningPartial = (
   layout,
 ) => {
-  // we hide the footer using display:none and then stickyFooter() in lightning.js will display it in the proper place
+  // we hide the footer using display:none and then stickyFooter() in lightning-tail.js will display it in the proper place
   // deno-fmt-ignore
   return `<footer class="footer font-size-medium" style="position: absolute; bottom: 0; height: 60px; margin-top: 40px; width: 100%; display:none;">
     <div class="slds-container_x-large slds-container_center slds-p-left_small slds-p-right_small">      
