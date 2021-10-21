d950442ea53300b4157148f59d7065049e7a0172
Shahid N. Shah <shahid@shah.org>
2021-10-21 12:26:43 -0400

    fix: revert Ajay's change, keep two columns
    Added style attr so that each cell remains two columns but fits text

--- core/design-system/lightning/layout/partial/index.ts
+++ core/design-system/lightning/layout/partial/index.ts
@@ -15,10 +15,10 @@ export const autoIndexCardsBodyPartial: ldsGovn.LightningPartial = (layout) => {
 
   // deno-fmt-ignore (because we don't want ${...} wrapped)
   return cardNodes
-    ? `<div class="slds-grid slds-wrap slds-var-m-around_medium">        
+    ? `<div class="slds-grid slds-wrap slds-var-m-around_medium" style="width:100%">
         ${cardNodes.filter(rtn => rtn !== layout.activeTreeNode).map(rtn => {
           const notifications = layout.dsArgs.navigation.descendantsNotifications(rtn);
-          return `<div class="slds-col slds-size_2-of-2 slds-order_2">
+          return `<div class="slds-col slds-size_1-of-2 slds-order_2">
             ${card.renderedCard(layout, {
               icon: { collection: "utility", name: rtn.children.length > 0 ? "open_folder": "page" },
               title: rtn.label,
