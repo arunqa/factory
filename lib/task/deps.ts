7f975e37e10b0e0e2d0ad2711aef8660989dc09a
Shahid N. Shah <shahid@shah.org>
2021-11-27 15:54:28 -0500

    feat: add download/fetch convenience task

--- lib/task/deps.ts
+++ lib/task/deps.ts
@@ -1,1 +1,2 @@
 export * as colors from "https://deno.land/std@0.114.0/fmt/colors.ts";
+export * as ioStreamConversion from "https://deno.land/std@0.114.0/streams/conversion.ts";
--- lib/task/fetch.ts
+++ lib/task/fetch.ts
@@ -1,4 +1,6 @@
+import { ioStreamConversion as iosc } from "./deps.ts";
 import * as govn from "./governance.ts";
+import * as human from "../text/human.ts";
 
 // deno-lint-ignore no-empty-interface
 export interface FetchTaskRunnerSupplier
@@ -47,6 +49,88 @@ export function graphQlTask(
         ),
       }).then((r) => r.json()).then(onData)
         .catch(onError);
+    },
+  };
+}
+
+// deno-lint-ignore no-empty-interface
+export interface DownloadTaskRunnerSupplier extends FetchTaskRunnerSupplier {
+}
+
+export const reportDownloadConsole = (
+  destFile: string,
+  srcEndpoint: string,
+  mark: PerformanceMark,
+) => {
+  const measure = performance.measure(mark.name, {
+    start: mark.startTime,
+  });
+  const info = Deno.statSync(destFile);
+  console.log(
+    `Downloaded ${destFile} (${
+      human.humanFriendlyBytes(info.size)
+    }) from ${srcEndpoint} (in ${measure.duration} ms)`,
+  );
+};
+
+// deno-lint-ignore require-await
+export async function downloadAsset(
+  srcEndpoint: string,
+  destFile: string,
+  onData?: (
+    destFile: string,
+    srcEndpoint: string,
+    mark: PerformanceMark,
+  ) => void,
+  onError?: (error: Error) => void,
+): Promise<void> {
+  const markName = `${srcEndpoint}::${destFile}`;
+  const mark = performance.mark(markName);
+  fetch(srcEndpoint).then(async (rsp) => {
+    try {
+      const rdr = rsp.body?.getReader();
+      if (rdr) {
+        const r = iosc.readerFromStreamReader(rdr);
+        const f = await Deno.open(destFile, {
+          create: true,
+          write: true,
+        });
+        await iosc.copy(r, f);
+        f.close();
+        if (onData) onData(destFile, srcEndpoint, mark);
+      }
+    } catch (err) {
+      if (onError) onError(err);
+    }
+  }).catch(onError);
+}
+
+export function downloadTask(
+  identity: string,
+  srcEndpoint: string,
+  destFile: string,
+  onData?: (
+    destFile: string,
+    srcEndpoint: string,
+    mark: PerformanceMark,
+  ) => void,
+  onError?: (error: Error) => void,
+): GraphQlTaskRunnerSupplier {
+  return {
+    identity,
+    exec: async () => {
+      if (!onError) {
+        onError = (error) => {
+          console.dir(error, { colors: true });
+        };
+      }
+
+      await downloadAsset(
+        srcEndpoint,
+        destFile,
+        onData || reportDownloadConsole,
+        onError,
+      );
     },
   };
 }
