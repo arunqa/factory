b9744dadbc9e992e03c30d49a616aabb1800cf57
Shahid N. Shah <shahid@shah.org>
2021-12-14 07:25:56 -0500

    feat: add console diagnostics

--- executive/typical/observability.ts
+++ executive/typical/observability.ts
@@ -1,5 +1,7 @@
+import * as colors from "https://deno.land/std@0.114.0/fmt/colors.ts";
 import * as rfGovn from "../../governance/mod.ts";
 import * as rfStd from "../../core/std/mod.ts";
+import * as rflHealth from "../../lib/health/mod.ts";
 
 declare global {
   interface Window {
@@ -13,3 +15,33 @@ if (!window.observability) {
     new rfGovn.ObservabilityEventsEmitter(),
   );
 }
+
+export const emitServiceHealthComponentsToConsole = async () => {
+  // any proxies or other components that register themselves will be iterable
+  const details = await window.observability.serviceHealthComponentsDetails();
+  for (const entry of Object.entries(details)) {
+    const [category, statuses] = entry;
+    for (const s of statuses) {
+      if (s.status === "pass") {
+        console.log(
+          colors.gray(`[${category}]`),
+          colors.green(`${s.componentId} ${colors.brightGreen(s.status)}`),
+        );
+      } else {
+        const status = s as rflHealth.UnhealthyServiceHealthComponentStatus;
+        console.log(
+          colors.gray(`[${category}]`),
+          `${
+            colors.red(`${status.componentId} ${colors.brightRed(s.status)}`)
+          } ${
+            Deno.inspect(status.links, {
+              colors: true,
+              depth: undefined,
+              compact: false,
+            })
+          } ${colors.gray(status.output)}`,
+        );
+      }
+    }
+  }
+};
