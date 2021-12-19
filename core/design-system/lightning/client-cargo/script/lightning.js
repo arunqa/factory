7fa941296432319d603f3f9d605da5ce8640f428
Shahid N. Shah <shahid@shah.org>
2021-12-19 11:29:58 -0500

    feat: add client-side word count (closes #66)

--- core/design-system/lightning/client-cargo/script/lightning.js
+++ core/design-system/lightning/client-cargo/script/lightning.js
@@ -403,3 +403,30 @@ customElements.define('time-ago',
   { extends: 'span' }
 );
 
+function getWordCount(rootElement) {
+  let docContent = rootElement.textContent;
+
+  // Parse out unwanted whitespace so the split is accurate
+  // ref: https://github.com/microsoft/vscode-wordcount/blob/main/extension.ts
+  docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
+  docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+  let wordCount = 0;
+  if (docContent != "") {
+    wordCount = docContent.split(" ").length;
+  }
+  return wordCount;
+}
+
+/**
+ * Create a custom element which will take any element ID and show how many words are in there.
+ * Usage in HTML:
+ *     <span is="word-count" element-id="ELEMENT_ID"/>
+ * `element-id` is a DOM element identifier like <div id="ELEMENT_ID">
+ */
+customElements.define('word-count',
+  class WordCountElement extends HTMLSpanElement {
+    static get observedAttributes() { return ['element-id']; }
+    connectedCallback() { this.innerHTML = getWordCount(document.getElementById("content")); }
+  },
+  { extends: 'span' }
+);
