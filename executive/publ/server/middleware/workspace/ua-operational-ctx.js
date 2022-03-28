4db357357e967b2c79f3b274fcaf86054effc7c4
Shahid N. Shah <shahid@shah.org>
2022-03-28 10:08:45 -0400

    refactor: only import lightbox if injecting

--- executive/publ/server/middleware/workspace/ua-operational-ctx.js
+++ executive/publ/server/middleware/workspace/ua-operational-ctx.js
@@ -1,25 +1,20 @@
 // this file is symlink'd by executive/publ/controller.ts:
 //
 // from executive/publ/server/middleware/workspace/ua-operational-ctx.js   (canonical)
-//   to public/operational-context/server.auto.mjs                          (reference)
+//   to public/operational-context/server.auto.mjs                         (reference)
 //
-// It's imported as a module by all pages when they run in Console mode by
-// pubctl.ts; it is run only by the browser, not at build time by Deno. If
-// you make changes, you can easily just hit browser refresh to reload.
+// It's imported as a module by all pages when they run in Experimental server mode by
+// pubctl.ts ("development mode"); it is run only by the browser, not at build time by
+// Deno. If you make changes, you can easily just hit browser refresh to reload.
 //
 // It's best to edit this file from public/operational-context/server.auto.mjs
 // rather than from executive/publ/server/middleware/workspace/ua-operational-ctx.js
 // because the deps are imported relative to public/operational-context/server.auto.mjs.
 
-// initExperimentalServerHooks should be called from a rfUniversalLayout.init() context
+// initExperimentalServerHooks should be called from a rfUniversalLayout.init() context.
 
 // d namespace contains Typescript-bundled dependencies
 import * as d from "./deps.auto.js";
-
-// https://github.com/biati-digital/glightbox is used for embedded Console
-// TODO[essential]: don't use @master, use @3.2.x specific
-//                  using @master now because of this bug: https://github.com/biati-digital/glightbox/pull/319
-import 'https://cdn.jsdelivr.net/gh/mcstudios/glightbox@master/dist/js/glightbox.min.js';
 
 // these are window-scoped so that child frames (e.g. Lightbox) can access;
 // the same exact variable names are created by RF frameset explorer.
@@ -29,45 +24,50 @@ function registerInspectionTarget(clientLayout, serverHooks) {
 }
 
 function injectRfExplorer(clientLayout, consoleNavContainerElem) {
-    const link = document.createElement('link');
-    link.href = "https://cdn.jsdelivr.net/gh/mcstudios/glightbox@master/dist/css/glightbox.css";
-    link.rel = "stylesheet";
-    document.head.appendChild(link);
+    // https://github.com/biati-digital/glightbox is used for embedded Console
+    // TODO[essential]: don't use @master, use @3.2.x specific
+    //                  using @master now because of this bug: https://github.com/biati-digital/glightbox/pull/319
+    import("https://cdn.jsdelivr.net/gh/mcstudios/glightbox@master/dist/js/glightbox.min.js").then(() => {
+        const glbStylesheet = document.createElement('link');
+        glbStylesheet.href = "https://cdn.jsdelivr.net/gh/mcstudios/glightbox@master/dist/css/glightbox.css";
+        glbStylesheet.rel = "stylesheet";
+        document.head.appendChild(glbStylesheet);
 
-    const activePath = clientLayout.navigation.location(clientLayout.route?.terminal);
-    const workspaceCtxURL = `/workspace${activePath == '/' ? '/' : `/index.html${activePath}`}`;
-    const gLightboxClassName = 'glightbox4';
-    const inspectorActivateHTML = `
-        <style>
-        .rfExplorerStrategies { display: none; }
-        .rfExplorerActivate:hover + .rfExplorerStrategies {
-            display: inline;
-            cursor:pointer;
-        }
-        .rfExplorerStrategies:hover {
-            display: inline;
-            cursor:pointer;
-        }
-        .rfExplorerActivate:hover {
-            cursor:pointer;
-        }
-        .rfExplorerStrategies a { text-decoration: none; }
-        </style>
-        <div style="padding-top: 4px">
-            <span class="rfExplorerActivate">🔭 rfExplorer</span><span class="rfExplorerStrategies">
-                <a href="/workspace/db/" class="${gLightboxClassName}" data-glightbox="width: ${window.innerWidth - 100}px; height: ${window.innerHeight - 100}px;" title="Open rfExplorer lightbox">🗔</a>
-                <a href="${workspaceCtxURL}?orientation=east&size=25" target="_top" title="Open rfExplorer panel to the right (East)">▶️</a>
-                <a href="${workspaceCtxURL}?orientation=south&size=25" target="_top" title="Open rfExplorer panel below (South)">🔽</a>
-                <a href="${workspaceCtxURL}?orientation=west&size=25" target="_top" title="Open rfExplorer panel to the left (West)">◀️</a>
-                <a href="${workspaceCtxURL}?orientation=north&size=25" target="_top" title="Open rfExplorer panel above (North)">🔼</a>
-            </span>
-        </div>`;
+        const activePath = clientLayout.navigation.location(clientLayout.route?.terminal);
+        const workspaceCtxURL = `/workspace${activePath == '/' ? '/' : `/index.html${activePath}`}`;
+        const gLightboxClassName = 'glightbox4';
+        const inspectorActivateHTML = `
+            <style>
+            .rfExplorerStrategies { display: none; }
+            .rfExplorerActivate:hover + .rfExplorerStrategies {
+                display: inline;
+                cursor:pointer;
+            }
+            .rfExplorerStrategies:hover {
+                display: inline;
+                cursor:pointer;
+            }
+            .rfExplorerActivate:hover {
+                cursor:pointer;
+            }
+            .rfExplorerStrategies a { text-decoration: none; }
+            </style>
+            <div style="padding-top: 4px">
+                <span class="rfExplorerActivate">🔭 rfExplorer</span><span class="rfExplorerStrategies">
+                    <a href="/workspace/db/" class="${gLightboxClassName}" data-glightbox="width: ${window.innerWidth - 100}px; height: ${window.innerHeight - 100}px;" title="Open rfExplorer lightbox">🗔</a>
+                    <a href="${workspaceCtxURL}?orientation=east&size=25" target="_top" title="Open rfExplorer panel to the right (East)">▶️</a>
+                    <a href="${workspaceCtxURL}?orientation=south&size=25" target="_top" title="Open rfExplorer panel below (South)">🔽</a>
+                    <a href="${workspaceCtxURL}?orientation=west&size=25" target="_top" title="Open rfExplorer panel to the left (West)">◀️</a>
+                    <a href="${workspaceCtxURL}?orientation=north&size=25" target="_top" title="Open rfExplorer panel above (North)">🔼</a>
+                </span>
+            </div>`;
 
-    consoleNavContainerElem.innerHTML = inspectorActivateHTML;
-    consoleNavContainerElem.style.display = "block"; // in case it was hidden
+        consoleNavContainerElem.innerHTML = inspectorActivateHTML;
+        consoleNavContainerElem.style.display = "block"; // in case it was hidden
 
-    // "teach" GLightbox which anchor should initiate the explorer
-    GLightbox({ selector: '.' + gLightboxClassName });
+        // "teach" GLightbox which anchor should initiate the explorer
+        GLightbox({ selector: '.' + gLightboxClassName });
+    });
 }
 
 function initExperimentalServerHooks(rfUniversalLayoutInitEvent) {
