dc787a283a6f3b836cd7af72d594bb6b95f3f776
Jijo M P <jijo.mp@citrusinformatics.com>
2022-05-09 09:24:39 +0530

    fix: update ENV variable for Matrix end point

--- core/design-system/lightning/layout/partial/ugc-comments-matrix.ts
+++ core/design-system/lightning/layout/partial/ugc-comments-matrix.ts
@@ -3,22 +3,36 @@ import * as ldsGovn from "../../governance.ts";
 import * as git from "../../../../../lib/git/mod.ts";
 import * as ws from "../../../../../lib/workspace/mod.ts";
 
+const ugcEnvVarsPrefix = "MATRIX_";
+
 export const ugcCommentsMatrix: ldsGovn.LightningPartial = (
   layout,
 ) => {
   const fm= layout?.frontmatter as any;
-  return `${fm?.ugc?.comments ?
+  const matrixServerNameEnvVar = `${ugcEnvVarsPrefix}SERVER_NAME`;
+  const matrixServerName = Deno.env.get(matrixServerNameEnvVar);
+  return `${matrixServerName && fm?.ugc?.comments ?
     `<div id="comment-section"></div>
 
     <script type="text/javascript" src="https://latest.cactus.chat/cactus.js"></script>
     <link rel="stylesheet" href="https://latest.cactus.chat/style.css" type="text/css">
-    <script src="/lightning/script/cactus-comments.js"></script>
     <script>
+    const domainDetails = new URL(window.location.href);
+    const pageUrlslug = domainDetails.pathname.replace(/\\//g, "-");
+    console.log(pageUrlslug);
+    const commentSectionId = pageUrlslug;
+    initComments({
+      node: document.getElementById("comment-section"),
+      defaultHomeserverUrl: "${Deno.env.get(`${ugcEnvVarsPrefix}SERVER_URL`)}",
+      serverName: "${matrixServerName}",
+      siteName: "${Deno.env.get(`${ugcEnvVarsPrefix}SITE_NAME`)}",
+      commentSectionId: commentSectionId,
+    });
       document.addEventListener("DOMContentLoaded", function () {
         // code
         console.log("loaded");
         document.querySelector(".cactus-send-button").innerHTML = "Post";
         document.querySelector(".cactus-logout-button").style.display = "none";
       });
-    </script>`: ""}`;
+    </script>`: `No ${matrixServerNameEnvVar} is defined, declare it in .envrc`}`;
 };
